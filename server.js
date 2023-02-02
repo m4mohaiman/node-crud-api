9cc48c7725920a919403835c91a2d146a6c8d73e
m4mohaiman <m4mohaiman@gmail.com>
2023-02-03 01:51:47 +0600

    data get by id

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get("/", (req, res) => {
 //create the data 
 app.post('/product', async (req ,res)=>{
     try{
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
+        const products = await Product.create(req.body);
+        res.status(200).json(products);
     } catch (error){
         console.log(error.message);
         res.status(500).json({message : error.message});
@@ -26,13 +26,26 @@ app.post('/product', async (req ,res)=>{
 //read all data 
 app.get('/product', async (req ,res)=>{
     try{
-        const product = await Product.find({});
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error){
+        console.log(error.message);
+        res.status(500).json({message : error.message});
+    }
+});
+
+//read data by ID
+app.get('/product/:id', async (req ,res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findById(id);
         res.status(200).json(product);
     } catch (error){
         console.log(error.message);
         res.status(500).json({message : error.message});
     }
 });
+
 
 
 
