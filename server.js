915d5839ffa101fc79437ff9842d9733ee0877fe
m4mohaiman <m4mohaiman@gmail.com>
2023-02-04 00:42:16 +0600

    update and delete init

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require('./models/productModel');
+var cors = require('cors')
 require('dotenv').config();
 const app = express();
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const port = 6969;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! Node");
+  res.send("API connected!");
 });
 
 
@@ -46,7 +48,37 @@ app.get('/product/:id', async (req ,res)=>{
     }
 });
 
+//edit data by ID 
+app.put('/product/:id', async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
 
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch(error){
+        res.status(500).json({message : error.message});
+    }
+});
+
+
+//delete data 
+app.delete('/product/:id', async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+    } catch(error){
+        res.status(500).json({message : error.message});
+    }
+});
 
 
 
